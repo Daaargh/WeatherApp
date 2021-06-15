dd2a263ca08a79876af0bb965f0d17b3fa3dcf2b
Daaargh <75450833+Daaargh@users.noreply.github.com>
2021-06-15 13:07:43 +0100

    Initial commit

---
+++ WeatherApp/View/WeatherWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WeatherApp.View
+{
+    /// <summary>
+    /// Interaction logic for Weather.xaml
+    /// </summary>
+    public partial class Weather : Window
+    {
+        public Weather()
+        {
+            InitializeComponent();
+        }
+    }
+}
